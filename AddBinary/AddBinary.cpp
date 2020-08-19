f440a72d527f241e55bef7eb4eac7921a97461f4
AnaZermeno <anazermeno19@gmail.com>
2020-08-19 17:39:38 -0600

    added execution file and base tasks in script

--- AddBinary/AddBinary.cpp
+++ AddBinary/AddBinary.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <sstream>
+#include <cmath>
 using namespace std;
 
 int main(){
-    
+
+    string string1 = "1010";
+    string string2 = "1011";
+
+    int n1 = 0, n2 = 0, cont = 0;
+     
+    //transformar de string1 binario a int decimal
+    for(int i = string1.size() - 1; i >= 0; i--){
+        int prov = (int)string1[i] - 48;
+        n1 = n1 + (prov * pow(2,cont));
+        cont++;
+    }
+
+    cont = 0;
+
+    //transformar de string2 binario a int decimal
+    for(int i = string2.size() - 1; i >= 0; i--){
+        int prov = (int)string2[i] - 48;
+        n2 = n2 + (prov * pow(2,cont));
+        cont++;
+    }
+
+    int temp = n1 + n2;
+
+    //transformar resultado decimal a binario
 }
